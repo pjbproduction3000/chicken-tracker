331e475f7dc3350200401b1cfae4a39dc07ea964
Barbara3000 <barbaraproductions3000@gmail.com>
2025-04-19 16:40:06 +0100

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/chicken_tracker/MainActivity.kt
@@ -0,0 +1,4 @@
+
+package com.example.chicken_tracker
+import io.flutter.embedding.android.FlutterActivity
+class MainActivity: FlutterActivity() {}
---
+++ lib/main.dart
@@ -0,0 +1,1 @@
+// Final main.dart - Chicken Tracker
